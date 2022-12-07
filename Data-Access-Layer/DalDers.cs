25ec5534b2feac1be08c30c823404f337ea5b926
Furkan Eralp Kahyaoğlu <furkan.kahyaoglu.3@gmail.com>
2022-12-07 18:59:22 +0300

    Dersler page created. Data access layer ders created.

---
+++ Data-Access-Layer/DalDers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using EntityLayer;
+
+namespace Data_Access_Layer
+{
+    public class DalDers
+    {
+        
+    }
+}
